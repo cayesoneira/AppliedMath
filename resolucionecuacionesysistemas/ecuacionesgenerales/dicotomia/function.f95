de0fbe2387e4cee71187d44d6245ba10e791b5df
cayesoneira <93153458+cayesoneira@users.noreply.github.com>
2022-01-27 12:32:49 +0100

    Delete resolucionecuacionesysistemas directory

--- resolucionecuacionesysistemas/ecuacionesgenerales/dicotomia/function.f95
+++
@@ -1,8 +0,0 @@
-function f(z) result (res)
-implicit none
-real,intent(in)::z
-real::res
-
-res=exp(-z)-z
-
-end function f
--- resolucionecuacionesysistemas/ecuacionesgenerales/iteracionfuncional/prin_iterfun.f95
+++
@@ -1,9 +0,0 @@
-program iterfun
-implicit none
-real::eps,nitmax,x0
-external::f
-
-call leciter(eps,nitmax,x0)
-call iter(f,eps,nitmax,x0)
-
-end program iterfun
